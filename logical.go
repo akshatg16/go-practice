d7ac91633f346a33eb2ceaa82b113cd77fb15fe7
akshatg16 <akshatg16@gmail.com>
2020-07-19 18:27:45 +0530

    initial commit for practice files

---
+++ logical.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+
+    fmt.Println(logical)
+
+    fmt.Println(true && false)
+    fmt.Println(false && true)
+    fmt.Println(true && true)
+    fmt.Println(false && false)
+    fmt.Println(true || false)
+    fmt.Println(false || true)
+    fmt.Println(true || true)
+    fmt.Println(false || false)
+    fmt.Println(true || false)
+    fmt.Println(!true)
+}
---
+++ recursive.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func fact(n int) int {
+    if n == 0 {
+        return 1
+    }
+    return n * fact(n-1)
+}
+
+func main() {
+    fmt.Println(fact(1))
+    fmt.Println(fact(2))
+    fmt.Println(fact(3))
+    fmt.Println(fact(4))
+    fmt.Println(fact(5))
+    fmt.Println(fact(6))
+    fmt.Println(fact(7))
+    fmt.Println(fact(8))
+    fmt.Println(fact(9))
+    fmt.Println(fact(10))
+    fmt.Println(fact(11))
+    fmt.Println(fact(12))
+
+}
